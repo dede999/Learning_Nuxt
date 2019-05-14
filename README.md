654e0b5919c4b6eb3a1dd16afa130562bb8bbf8d
dede999 <andreluizsa00@gmail.com>
2019-05-13 22:48:56 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Learning Nuxt JS
+
+> My sublime Nuxt.js project
+
+Learning Nuxt on my spare time, trough Vue school course. 
+Probably gonna need some help.
+
+## Build Setup
+
+``` shell
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn run dev
+
+# build for production and launch server
+$ yarn run build
+$ yarn start
+
+# generate static project
+$ yarn run generate
+```
+
+For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
---
+++ pages/posts/_id.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-container>
+       <h1 class="display-1"> Post #{{ post.id }} </h1>
+
+       <v-layout row-wrap>
+         <v-flex s12>
+            <v-card color="blue-red" class="white--text">
+               <v-card-title>
+                  {{ post.title }}
+               </v-card-title>
+               {{ post.description }}
+            </v-card>
+         </v-flex>
+       </v-layout>
+    </v-container>
+</template>
+
+<script>
+    import post from "./post";
+
+    export default {
+       name: "_id.vue",
+       data() {
+          return {
+             id: this.$route.params.id,
+             posts: [
+                {id: 1, title: "Feeding Rex", description: "Learn how to feed your dog"},
+                {id: 2, title: "Breeding Rex", description: "Learn how to help your pal finding his/her soul mate"},
+                {id: 3, title: "Adopting", description: "Where should I adopt my dog"}
+             ]
+          }
+       },
+       computed: {
+          post () {
+             return this.posts.find(post => post.id === this.id)
+          }
+       }
+    }
+</script>
+
+<style scoped>
+
+</style>
